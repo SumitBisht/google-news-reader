d81f935321a86029686b2e43ef2c8768027becd1
sumitbisht <sumitbisht1987@gmail.com>
2014-02-19 12:53:57 +0530

    added postgres as backend db for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140217105755) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "news", force: true do |t|
     t.string   "title"
     t.string   "url"
